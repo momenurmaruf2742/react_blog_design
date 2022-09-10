8aa4f3288b7e17976cdce920431526c2c914dfd8
momenurmaruf2742 <momenur.maruf@gmail.com>
2022-09-10 21:22:26 +0600

    Contex working

---
+++ client/src/context/Context.js
@@ -0,0 +1,15 @@
+import { createContext,useReducer } from "react"
+
+
+const INITIAL_STATE = {
+    user:null,
+    isFetching:false,
+    error:false,
+}
+
+export const Context = createContext(INITIAL_STATE);
+
+
+export const ContextProvider = ({children}) => {
+    
+}
---
+++ client/src/context/Reducer.js
@@ -0,0 +1,27 @@
+const Reducer = (state,action)=>{
+    switch(action.type){
+        case "LOGIN_START":
+            return{
+                user:null,
+                isFetching:true,
+                error:false
+            };
+            case "LOGIN_START":
+                return{
+                    user:null,
+                    isFetching:true,
+                    error:false
+                };
+            case "LOGIN_START":
+                return{
+                    user:null,
+                    isFetching:true,
+                    error:false
+                };
+            default:
+                return state;
+                 
+         
+    }
+}
+
